19ac3490a1eee57c6d2697e825ac7f1c87c79fbd
frankabras <frankabras@users.noreply.github.com>
2026-01-14 22:00:28 +0100

    wip: example of logging file added

---
+++ Codes/logging_example.py
@@ -0,0 +1,11 @@
+# -------------------------------------------------------------------------------------------
+# File : logging.py
+# Desc : This file contains a dictionary with sensitive information such as 
+#        passwords and API keys.
+# -------------------------------------------------------------------------------------------
+
+my_logging = {
+    "ssid" : "Your_WiFi_SSID",
+    "WiFi_pass" : "Your_WiFi_Password",
+    "OWM_API_key" : "Your_OWM_API_Key",
+}
